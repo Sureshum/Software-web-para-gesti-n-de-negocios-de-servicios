654ddbe038dff76a20735d8fa549dd2108055e8d
Sebastián Shum <89267654+Sureshum@users.noreply.github.com>
2026-07-23 18:48:50 -0400

    Delete src/orders directory

--- src/orders/dto/create-order.dto.ts
+++
@@ -1,8 +0,0 @@
-export class CreateOrderDto {
-  tenantId: number;
-  clientId?: number;
-  assignedTo?: number;
-  status?: 'pending' | 'in_progress' | 'completed' | 'cancelled';
-  description: string;
-  totalCost?: number;
-}
