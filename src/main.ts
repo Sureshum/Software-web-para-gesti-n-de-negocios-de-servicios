09113522d9c9d53e1a466c1dc8ba35d97afb33f2
Sebastián Shum <89267654+Sureshum@users.noreply.github.com>
2026-07-23 18:37:42 -0400

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
 
-  const port = (globalThis as any).process?.env?.PORT || 3000;
+  app.useStaticAssets(join(__dirname, '..', 'front'), {
+    prefix: '/',
+  });
+
+  const port = process.env.PORT || 3000;
   await app.listen(port, '0.0.0.0');
   
   console.log(`Application is running on port: ${port}`);
